15535222bfb7ad3accf4a678d58a172d8d91d874
Rachel Clark <rachel.mae.clark@gmail.com>
2013-08-27 00:07:09 -0400

    Added MotorCommand publishing in the PID node

--- Applications/corobot_pid/node/corobot_pid_node.py
+++ Applications/corobot_pid/node/corobot_pid_node.py
@@ -4,26 +4,40 @@ import rospy
 from std_msgs.msg import Float32
 from std_msgs.msg import Int32
 from corobot_msgs.msg import MotorCommand
+from math import sin
+
+# Global variables
+target_velocity = None
+motor_pub = None
+
 
 def node():
+    global target_velocity, motor_pub
     rospy.init_node('cpid')
-    pub = rospy.Publisher('control_error', Float32)
-    pub = rospy.Publisher('target_velocity', Int32)
-    pub = rospy.Publisher('PhidgetMotor', MotorCommand)
-    while not rospy.is_shutdown():
-	mc = MotorCommand()
-	mc.leftSpeed = 50
-	mc.rightSpeed = 50
-	mc.acceleration = 50
-	mc.secondsDuration = 1
-        pub.publish(mc)
-        rospy.sleep(1.0)
+    error_pub = rospy.Publisher('control_error', Float32)
+    rospy.Subcriber('target_velocity', Int32, set_target_velocity)
+    motor_pub = rospy.Publisher('PhidgetMotor', MotorCommand)
+    rospy.Subcriber('control_correction', Float32, send_command)
+
+    rospy.spin()
+
+
+def set_target_velocity(target):
+    global target_velocity
+    target_velocity = target
+
+
+def send_command(adjustment):
+    global target_velocity, motor_pub
+    mc = MotorCommand()
+    mc.leftSpeed = target_velocity * sin(adjustment)
+    mc.rightSpeed = target_velocity * (1 - sin(adjustment))
+    mc.acceleration = 50
+    mc.secondsDuration = 1
+    motorPub.publish(mc)
 
 if __name__ == '__main__':
     try:
-        talker()
+        node()
     except rospy.ROSInterruptException:
         pass
-
-
-
